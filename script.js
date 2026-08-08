347c4d756f5b088f2442b10fdf5e34117b6fc3bf
hyder4056 <47360441+hyder4056@users.noreply.github.com>
2026-08-08 11:16:25 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    let name = document.getElementById("name").value;
+
+    document.getElementById("result").innerHTML =
+        "Thank you " + name + ". Your message has been submitted.";
+
+});
+
